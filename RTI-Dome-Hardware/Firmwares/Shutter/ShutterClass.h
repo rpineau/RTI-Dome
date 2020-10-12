39b402f09c066ad02d1124a9883dfb923b0a723a
Rodolphe Pineau <pineau@rti-zone.org>
2020-10-12 16:15:51 -0700

    More code cleanup

--- RTI-Dome-Hardware/Firmwares/Shutter/ShutterClass.h
+++ RTI-Dome-Hardware/Firmwares/Shutter/ShutterClass.h
@@ -4,12 +4,6 @@
 //
 
 
-
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "arduino.h"
-#else
-#include "WProgram.h"
-#endif
 #if defined ARDUINO_DUE
 #include <DueFlashStorage.h>
 DueFlashStorage dueFlashStorage;
