45b3559aea5603917f39d589e7af3d1473b1cfeb
Rodolphe Pineau <pineau@rti-zone.org>
2020-10-31 16:04:26 -0700

    DTR not needed on the DUE.. this will make sure people don't try to use and AVR and complain things are not working. Of course this doesn't quite solve the case of macOS that still assert DTR and reset the controlle :(

--- RTI-Dome.cpp
+++ RTI-Dome.cpp
@@ -118,7 +118,8 @@ int CRTIDome::Connect(const char *pszPort)
 #endif
 
     // 9600 8N1
-    nErr = m_pSerx->open(pszPort, 115200, SerXInterface::B_NOPARITY, "-DTR_CONTROL 1");
+    // nErr = m_pSerx->open(pszPort, 115200, SerXInterface::B_NOPARITY, "-DTR_CONTROL 1");
+    nErr = m_pSerx->open(pszPort, 115200, SerXInterface::B_NOPARITY);
     if(nErr) {
         m_bIsConnected = false;
         return nErr;
