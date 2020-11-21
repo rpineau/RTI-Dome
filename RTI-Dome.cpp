98a1449cde9bf34afe7bf7c7f26d94305c5408f2
Rodolphe Pineau <pineau@rti-zone.org>
2020-11-21 15:59:34 -0800

    Better fix for the homing retry

--- RTI-Dome.cpp
+++ RTI-Dome.cpp
@@ -1531,9 +1531,11 @@ int CRTIDome::isFindHomeComplete(bool &bComplete)
         if(m_nHomingTries == 0) {
             m_nHomingTries = 1;
             goHome();
-            return PLUGIN_OK;
         }
-        return MAKE_ERR_CODE(PLUGIN_ID, DriverRootInterface::DT_DOME, ERR_CMDFAILED);
+        else {
+            m_nHomingTries = 0;
+            nErr = MAKE_ERR_CODE(PLUGIN_ID, DriverRootInterface::DT_DOME, ERR_CMDFAILED);
+        }
     }
 
     return nErr;
