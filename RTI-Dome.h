7f70a8e7139fa61ed9be434c4937d66427c88195
Rodolphe Pineau <pineau@rti-zone.org>
2021-03-29 07:59:14 -0700

    Remove the Reconnect code as it will not work as is and we're trying to find the issue in TSX related to TCP.

--- RTI-Dome.h
+++ RTI-Dome.h
@@ -67,7 +67,6 @@ public:
     ~CRTIDome();
 
     int         Connect(const char *pszPort);
-    int         Reconnect();
     void        Disconnect(void);
     const bool  IsConnected(void) { return m_bIsConnected; }
 
