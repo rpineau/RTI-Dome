8dd705923e0de6182bda0e3bbdc55a28ed4cb3a5
Rodolphe Pineau <pineau@rti-zone.org>
2021-08-21 17:57:07 -0700

    Fix typo

--- Hardware/Firmwares/RotatorEth/RotatorEth.ino
+++ Hardware/Firmwares/RotatorEth/RotatorEth.ino
@@ -2,7 +2,7 @@
 // RTI-Zone Dome Rotator firmware. Based on https://github.com/nexdome/Automation/tree/master/Firmwares
 // As I contributed to the "old" 2,x firmware and was somewhat familiar with it I decided to reuse it and
 // fix most of the known issues. I also added some feature related to XBee init and reset.
-// This is meant to run on an Arduino DUE as we put he AccelStepper run() call in an interrupt
+// This is meant to run on an Arduino DUE as we put the AccelStepper run() call in an interrupt
 //
 
 // Uncomment #define DEBUG in RotatorClass.h to enable printing debug messages in serial
